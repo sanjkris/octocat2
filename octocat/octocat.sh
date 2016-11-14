e95b67e7b16275e51a8fa51c086884c2bc7c9f34
Ellen <eclinester@gmail.com>
2016-11-14 21:54:14 +0000

    working on our script!

--- octocat/octocat.sh
+++ octocat/octocat.sh
@@ -36,13 +36,42 @@ printf "Today is %s\n" "$(date)"
 #Print unique identifier last
 echo "User ID = $ID"
 
+#Say goodbye
+echo "Thank you for your answers!"
+
 #Put it all in a .csv file
 answers=`echo "$varname,$varage,$varclass,$varanim,$varcol,$(date),$ID"`
 echo "$answers" >> data.csv
 
-#Say goodbye
-echo "Thank you for your answers! Goodbye!"
+#Username = ?
+echo "What's your MySQL username?"
+read user
 
+read -p "About to use MySQL. Would you like to continue?"
+mysql -u $user -e "show databases"
 
+#You never know! This might be useful later
+#if [ $MYSQL_PASS ]
+#then
+#  mysql -u "$MYSQL_ROOT" -p "$MYSQL_PASS" -e "SHOW DATABASES"
+#else
+#  mysql -u "$MYSQL_ROOT" -e "SHOW DATABASES"
+#fi
+
+#read -p "About to enter MySQL. Would you like to continue?"
+#mysql -u $username -p $password -e "show databases"
+ 
+#Hahaha this didn't work
+#mysql-ctl cli
+#USE octocat;
+#LOAD DATA INFILE '/home/ubuntu/workspace/octocat2/octocat/data.csv' INTO TABLE octocat FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+#SELECT * FROM octocat ORDER BY Date; 
+#quit
+
+#All data added to database "octocat"
+echo "Thank you for your addition to our database!"
+
+#PS This is a test do we really need it???
+#mysqldump -u efcline -p octocat > octocat.sql
 
 
